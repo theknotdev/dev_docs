1a01d024226e49dcaef8a1a829c64ff35fdaa140
theknotdev <theknotdev@gmail.com>
2025-07-23 16:54:12 +0800

    feat: add launch configurations for Next.js debugging and document container usage on macOS

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [new URL("https://*.guanglai.me/**")],
+  },
 };
 
 export default withMDX(config);
