c1fbd03328910bf6b57f62b299b42a76e8cd5c9d
theknotdev <theknotdev@gmail.com>
2025-07-23 10:42:20 +0800

    feat: add @orama/tokenizers dependency and update search route to use tokenizer component

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,7 +1,14 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
+import { source } from "@/lib/source";
+import { createFromSource } from "fumadocs-core/search/server";
+import { createTokenizer } from "@orama/tokenizers/mandarin";
 
 export const { GET } = createFromSource(source, {
   // https://docs.orama.com/open-source/supported-languages
-  language: 'english',
+  components: {
+    tokenizer: createTokenizer(),
+  },
+  search: {
+    threshold: 0,
+    tolerance: 0,
+  },
 });
